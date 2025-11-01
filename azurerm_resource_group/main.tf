3e0b3d3b6f3b63f61f8abf8a3fd4179cdd6f2a2d
Mohit Chauhan <mohitchauhan.devops@gmail.com>
2025-11-01 16:25:47 +0530

    add rg102

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "git_ka_rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg4" {
+  name     = "git_ka_rg102"
+  location = "West Europe"
+}
