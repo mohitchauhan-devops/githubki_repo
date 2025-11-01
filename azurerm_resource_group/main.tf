b39e41891a369065d8b08d095115893e8ceb3489
Mohit Chauhan <mohitchauhan.devops@gmail.com>
2025-11-01 18:23:03 +0530

    all resource deleted

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -1,19 +1,1 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "git_ka_rg"
-  location = "West Europe"
-}
 
-
-resource "azurerm_resource_group" "rg3" {
-  name     = "git_ka_rg101"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "rg6" {
-  name     = "git_ka_rg105"
-  location = "West Europe"
-
-  tags = {
-    environment = "dev"
-  }
-}
