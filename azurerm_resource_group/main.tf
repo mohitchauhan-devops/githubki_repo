ba235036a62f11759b7da6706f9ff35c01359638
Mohitchauhan-QA <mohitchauhan.devops@gmail.com>
2025-11-05 18:58:28 +0530

    new stg

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -6,3 +6,14 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+resource "azurerm_storage_account" "stg123456" {
+  name                     = "scarlett"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
