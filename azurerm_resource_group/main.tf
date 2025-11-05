5639aec75da7bc868d0dc6417f31bae995b2c53a
Mohitchauhan-QA <mohitchauhan.devops@gmail.com>
2025-11-05 18:56:32 +0530

    sdgsdgsyd

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -6,14 +6,3 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "scarlett"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
